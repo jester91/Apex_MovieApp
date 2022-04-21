89e8422626789b6622872bd1b6fa2d2c2d3f02d4
Konrad Kovacs <konrad.kovacs97@outlook.com>
2022-04-21 23:53:50 +0200

    MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 //import
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "@mui/material/Button";
 import Movie from "./components/Movie";
 import React, { useEffect, useState } from "react";
+import { TextField, Input } from "@mui/material";
 
 //init the api
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
-// const recommended_api="https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1"
-// const img_api="https://image.tmdb.org/t/p/w1280"
-// const search_api=""
 
 const API_URL = "https://api.themoviedb.org/3/";
 
@@ -19,37 +16,56 @@ const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`;
 
 const FILTER_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&page=5`;
 
-const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-
-const BACKDROP_SIZE = "w1280";
-
-const POSTER_SIZE = "w500";
-
-export {
-  SEARCH_BASE_URL,
-  POPULAR_BASE_URL,
-  FILTER_BASE_URL,
-  API_URL,
-  API_KEY,
-  IMAGE_BASE_URL,
-  BACKDROP_SIZE,
-  POSTER_SIZE,
-};
+export { SEARCH_BASE_URL, POPULAR_BASE_URL, FILTER_BASE_URL, API_URL, API_KEY };
 
 //core app
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(POPULAR_BASE_URL)
+    getMovies(POPULAR_BASE_URL);
+  }, []);
+
+  const getMovies =(API) =>{
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, []);
+  }
 
-  return <div>{movies.length > 0 && movies.map((movie) => <Movie key={movie.id}  data={movie}/>)}</div>;
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_BASE_URL + searchTerm);
+
+      setSearchTerm("");
+    }
+  };
+
+  const handleonchange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            placeholder="Search for movie"
+            className="search-box"
+            value={searchTerm}
+            onChange={handleonchange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const Movie =() => (
-    <div classname="movie">This will be the movie component</div>
-)
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const Movie = ({
+  title,
+  poster_path,
+  overview,
+  vote_average,
+  backdrop_path,
+}) => (
+  <div className="movie">
+    <img src={IMAGE_BASE_URL + backdrop_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
+    <div className="movie-effect">
+    <h3>Overview</h3>
+    <p>{overview}</p>
+    </div>
+  </div>
+);
 
 export default Movie;
